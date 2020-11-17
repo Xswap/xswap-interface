cd913c1eba470e745c9fa39fb118d1e0415e1bae
Oleksandr Dyomin <o.o.dyomin@gmail.com>
2020-11-17 02:00:38 +0200

    Use @eliteswap npm packages instead of @uniswap (except token-lists and merkle-distributor)

--- src/utils/prices.test.ts
+++ src/utils/prices.test.ts
@@ -1,4 +1,4 @@
-import { ChainId, JSBI, Pair, Route, Token, TokenAmount, Trade, TradeType } from '@uniswap/sdk'
+import { ChainId, JSBI, Pair, Route, Token, TokenAmount, Trade, TradeType } from '@eliteswap/sdk'
 import { computeTradePriceBreakdown } from './prices'
 
 describe('prices', () => {
