9181f235c384951da110981cfd2dc30d45e9094f
Lint Action <lint-action@samuelmeuli.com>
2020-11-18 13:14:55 +0000

    Fix code style issues with ESLint

--- src/constants/lists.ts
+++ src/constants/lists.ts
@@ -1,5 +1,6 @@
 // the Elite Swap Default token list lives here
-export const DEFAULT_TOKEN_LIST_URL = 'https://unpkg.com/@eliteswap/default-token-list@1.5.0/build/eliteswap-default.tokenlist.json'
+export const DEFAULT_TOKEN_LIST_URL =
+  'https://unpkg.com/@eliteswap/default-token-list@1.5.0/build/eliteswap-default.tokenlist.json'
 
 export const DEFAULT_LIST_OF_LISTS: string[] = [
   DEFAULT_TOKEN_LIST_URL,
